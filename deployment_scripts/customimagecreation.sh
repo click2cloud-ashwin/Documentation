0b50fc92b1f8cb3ec009b4255d015fa401acc572
Click2Cloud-rninja <roshan.wanaskar@click2cloud.net>
2021-09-06 14:52:44 +0530

    updated custom image script

--- deployment_scripts/customimagecreation.sh
+++ deployment_scripts/customimagecreation.sh
@@ -12,16 +12,16 @@ gcloud compute instances create $INSTANCENAME_FINAL \
         --image-project=ubuntu-os-cloud \
         --maintenance-policy=TERMINATE \
         --machine-type=$INSTANCE_TYPE \
-	--image-project=ubuntu-os-cloud \
-	--metadata=startup-script='#! /bin/bash
+        --image-project=ubuntu-os-cloud \
+        --metadata=startup-script='#! /bin/bash
 apt remove ifupdown
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sed -i 's/ens4/eth0/g' /etc/netplan/50-cloud-init.yaml
 sudo netplan generate && netplan apply
 echo "Temporary instance is creating"'
-gcloud auth login
+#gcloud auth login
 echo "Creating custom image"
 gcloud compute images create $CUSTOMIMAGE --source-disk=$INSTANCENAME_FINAL --source-disk-zone=us-central1-a --source-disk-project=click2cloud --family=ubuntu-os-cloud --force
 echo "Deleting temporary instance"
-gcloud compute instances delete $INSTANCENAME_FINAL -q
+gcloud compute instances delete $INSTANCENAME_FINAL -q --zone=$ZONE
